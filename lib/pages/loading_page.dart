5317305357ef8b78533722bd3e0acb300d8e3aac
Luis Vice <khanccer@gmail.com>
2020-10-24 23:29:35 -0500

    version 0.5

--- lib/pages/loading_page.dart
+++ lib/pages/loading_page.dart
@@ -5,8 +5,19 @@ class LoadingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('LoadingPage'),
+        child: CircularProgressIndicator(
+          strokeWidth: 2,
+        ),
       ),
     );
+  }
+
+  Future checkGpsYLocation(BuildContext context) async {
+    // Permiso GPS
+
+    // GPS esta activo
+
+    await Future.delayed(Duration(milliseconds: 1000));
+    Navigator.pushReplacementNamed(context, 'mapa');
   }
 }
